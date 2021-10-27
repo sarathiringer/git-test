7b2320f65504af95538b57e7819f64fce04b8205
Sara Thiringer <s.thiringer@gmail.com>
2021-10-27 06:51:52 +0200

    Add ggplot2 graph

--- penguins_script.R
+++ penguins_script.R
@@ -1,8 +1,13 @@
 # Load packages
 library(palmerpenguins)
+library(ggplot2)
 
 # Fetch data
 data("penguins")
 
 # Inspect data
 head(penguins)
+
+ggplot(penguins, aes(x = bill_length_mm, bill_depth_mm, col = island)) +
+  geom_point() +
+  geom_smooth()
